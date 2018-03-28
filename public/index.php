256df5cc9c4b36ad16f34ba3d62ac3e80c88fea6
rwhite53 <rwhite@october-design.com>
2018-03-28 16:37:21 -0400

    DemoApp initial configuration, displays default ZendFramework welcome

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use Zend\Mvc\Application;
+use Zend\Stdlib\ArrayUtils;
+
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
+}
+
+// Composer autoloading
+include __DIR__ . '/../vendor/autoload.php';
+
+if (! class_exists(Application::class)) {
+    throw new RuntimeException(
+        "Unable to load application.\n"
+        . "- Type `composer install` if you are developing locally.\n"
+        . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
+        . "- Type `docker-compose run zf composer install` if you are using Docker.\n"
+    );
+}
+
+// Retrieve configuration
+if (!defined('APPLICATION_PATH')) {
+    define('APPLICATION_PATH', realpath(__DIR__ . '/../'));
+}
+
+// configs for development and production
+$appConfig = include APPLICATION_PATH . '/config/application.config.php';
+
+// configs for development only
+if (file_exists(APPLICATION_PATH . '/config/development.config.php')) {
+    $appConfig = Zend\Stdlib\ArrayUtils::merge($appConfig, include APPLICATION_PATH . '/config/development.config.php');
+}
+
+// Run the application!
+Zend\Mvc\Application::init($appConfig)->run();
